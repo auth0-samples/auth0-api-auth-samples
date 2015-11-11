912da84d8438e8f29ff694742c4f5c8e5c3fde73
Sandrino Di Mattia <contact@sandrino.be>
2015-11-11 22:49:05 +0100

    Added user consent seed projects

---
+++ user-consent/calendarapp-implicit-jquery/index.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var path = require('path');
+var morgan = require('morgan');
+var logger = require('./logger');
+
+var express = require('express');
+var hbs = require('express-hbs');
+var app = express();
+app.use(morgan(':method :url :status :response-time ms - :res[content-length]', {
+	stream: logger.stream
+}));
+
+app.use(express.static('public'));
+app.engine('hbs', hbs.express4());
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+var nconf = require('nconf');
+nconf.env()
+  .file({ file: './config.json' });
+
+var callback_url = "http://localhost:7002";
+var authorize_url = "https://{AUTH0_DOMAIN}/i/oauth2/authorize?scope=appointments%20contacts&response_type=token&client_id={AUTH0_CLIENT_ID}&redirect_uri={CALLBACK_URL}"
+	.replace(/({AUTH0_DOMAIN})/g, nconf.get('AUTH0_DOMAIN'))
+	.replace(/({AUTH0_CLIENT_ID})/g, nconf.get('AUTH0_CLIENT_ID'))
+	.replace(/({CALLBACK_URL})/g, callback_url);
+
+/*
+* Serve HTML page.
+*/
+app.get('/', function(req, res, next) {
+  res.render('index', {
+		callback_url: callback_url,
+    authorize_url: authorize_url,
+		auth0_domain: nconf.get('AUTH0_DOMAIN'),
+		auth0_client_id: nconf.get('AUTH0_CLIENT_ID')
+  });
+});
+
+/*
+ * Start server.
+ */
+http.createServer(app).listen(7002, function() {
+	logger.info('CalendarApp (Client) listening on: http://localhost:7002/');
+});
---
+++ user-consent/organizer-nodejs/index.js
@@ -0,0 +1,85 @@
+var fs = require('fs');
+var jwt = require('express-jwt');
+var http = require('http');
+var cors = require('cors');
+var morgan = require('morgan');
+var logger = require('./logger');
+var publicKey = fs.readFileSync('./key.pem');
+
+var express = require('express');
+var app = express();
+app.use(cors());
+app.use(morgan(':method :url :status :response-time ms - :res[content-length]', {
+	stream: logger.stream
+}));
+
+var nconf = require('nconf');
+nconf.env()
+  .file({ file: './config.json' });
+
+/*
+ * Middleware that will validate the incoming access token.
+ */
+var jwtCheck = jwt({
+  secret: publicKey,
+  audience: nconf.get('RESOURCE_SERVER'),
+  issuer: 'https://' + nconf.get('AUTH0_DOMAIN') + '/'
+});
+
+/*
+ * Middleware that checks if a scope is available in the current user.
+ */
+var requireScope = function(expected_scope){
+  return function (req, res, next){
+    if (!req.user || !req.user.scope.includes(expected_scope)){
+      return next(new Error('Cannot perform action. Missing scope ' + expected_scope));
+    }
+    next();
+  };
+};
+
+/*
+ * API endpoints.
+ */
+app.use('/api', jwtCheck, function(req, res, next) {
+  if (req.user) {
+		logger.debug('Current user: ' + req.user.sub + ' (scope=' + req.user.scope + ')');
+	}
+  next();
+});
+app.get('/api/appointments', requireScope('appointments'), function(req, res, next) {
+	res.json([
+    { subject: 'Meet with Fabrikam', date: '2015-11-20' },
+    { subject: 'Follow up on deal with Contoso', date: '2015-12-03' }
+  ]);
+});
+app.get('/api/contacts', requireScope('contacts'), function(req, res, next) {
+	res.json([
+    { name: 'John Doe', email: 'john.doe@gmail.com' },
+    { name: 'Jane Doe', email: 'jane.doe@gmail.com' }
+  ]);
+});
+app.get('/api/tasks', requireScope('tasks'), function(req, res, next) {
+	res.json([
+    { title: 'Finish blog post', due_date: '2015-12-05' }
+  ]);
+});
+
+/*
+ * Error handler
+ */
+app.use(function(err, req, res, next) {
+  if (err) {
+    logger.error('Unauthorized:', err.message);
+    return res.status(401).send({ message: err.message });
+  }
+
+  next(err, req, res);
+});
+
+/*
+ * Start server.
+ */
+http.createServer(app).listen(7001, function() {
+	logger.info('Organizer API (Resource Server) listening on: http://localhost:7001');
+});
